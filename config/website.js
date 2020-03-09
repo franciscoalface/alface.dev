9149455b0f117363abc9f5a40c1c93e6ebb45cd7
Francisco Alface <falface@gmail.com>
2020-03-08 22:27:55 -0300

    Initial commit of my first gatsby blog

---
+++ config/website.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Alface.dev', // Navigation and Site Title
+  siteTitleAlt: 'The blog of a software developer named Francisco Alface', // Alternative Site title for SEO
+  siteTitleShort: 'Alface.dev', // short_name for manifest
+  siteUrl: 'https://alface.dev', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'This is where I post things!',
+  author: 'Francisco Alface', // Author for schemaORGJSONLD
+  organization: '',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@FranciscoAlface', // Twitter Username
+  ogSiteName: 'Francisco Alface', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#215732',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitterUrl: 'https://twitter.com/FranciscoAlface/',
+  twitterHandle: '@FranciscoAlface',
+  githubUrl: 'https://github.com/franciscoalface/',
+  githubHandle: 'franciscoalface',
+  fbAppID: ''
+};
---
+++ src/@eggheadio/gatsby-theme-egghead-blog/lib/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black: '#131415',
+  white: '#fff',
+  gray: '#fafafa',
+  red: '#E74C3C',
+  blue: '#5348ff',
+  green: '#29B573',
+  node: '#215732'
+};
+
+export default colors;
